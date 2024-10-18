12debceb3ffbc4071a7b1194b315f3710195ff6b
GENRAY1 <genray@ro.ru>
2024-10-18 18:36:05 +0300

    init

---
+++ backend/TokenTrends.Application/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace TokenTrends.Application;
+
+public class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
---
+++ backend/TokenTrends.Web/Program.cs
@@ -0,0 +1,25 @@
+using TokenTrends.Web.Extentions;
+using TokenTrends.Web.Middaleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerServices();
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
